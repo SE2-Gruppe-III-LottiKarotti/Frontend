de3390b7e74e1c0cffbae99aa20d8c00a4de8511
Vojticek Florian (IT OS WP ADO OD) <Florian.Vojticek@infineon.com>
2024-05-26 15:16:29 +0200

    Add Card3 for drawing

--- app/src/main/java/at/aau/serg/websocketdemoapp/fragments/Rabbit3.java
+++ app/src/main/java/at/aau/serg/websocketdemoapp/fragments/Rabbit3.java
@@ -3,64 +3,32 @@ package at.aau.serg.websocketdemoapp.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import at.aau.serg.websocketdemoapp.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Rabbit3#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Rabbit3 extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Rabbit3() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Rabbit3.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Rabbit3 newInstance(String param1, String param2) {
-        Rabbit3 fragment = new Rabbit3();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rabbit3, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_rabbit3, container, false);
+
+        ImageButton closeButton = view.findViewById(R.id.close_button);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getParentFragmentManager();
+                fragmentManager.beginTransaction().remove(Rabbit3.this).commit();
+            }
+        });
+        return view;
     }
 }
