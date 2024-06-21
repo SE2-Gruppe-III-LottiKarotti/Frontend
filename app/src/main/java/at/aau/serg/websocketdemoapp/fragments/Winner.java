9e844ec6ca7153e12472fbb7aa295e03eef37a16
Vojticek Florian (IT OS WP ADO OD) <Florian.Vojticek@infineon.com>
2024-06-21 15:56:12 +0200

    Add new fragment for winner

---
+++ app/src/main/java/at/aau/serg/websocketdemoapp/fragments/Winner.java
@@ -0,0 +1,32 @@
+package at.aau.serg.websocketdemoapp.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import at.aau.serg.websocketdemoapp.R;
+import at.aau.serg.websocketdemoapp.activities.MainActivity;
+
+public class Winner extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_winner, container, false);
+
+        ImageButton backButton = view.findViewById(R.id.back_button);
+        backButton.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), MainActivity.class);
+            startActivity(intent);
+        });
+        return view;
+    }
+}
